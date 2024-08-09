3d78c7292b7047696e3990a3566884f5cc9e3fb7
root <you@example.com>
2024-08-09 09:33:34 +0000

    commit

--- sort.h
+++ sort.h
@@ -8,9 +8,9 @@
 #define UP 0
 #define DOWN 1
 
-/*
+/**
  * enum bool - Enumeration of boolean values
- * */
+ */
 typedef enum bool
 {
 	false = 0,
@@ -32,9 +32,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*Helper for swapping*/
-void swap_ints(int *a, int *b);
-
 /*Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -44,7 +41,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-
-
 #endif
